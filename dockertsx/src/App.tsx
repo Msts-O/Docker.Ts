98373f0d4e94fba45f4ead4492adc790b7924f55
Masatoshi <msts.oo0131@gmail.com>
2021-02-27 18:26:58 +0900

    map.name.created

--- dockertsx/src/App.tsx
+++ dockertsx/src/App.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
-import './App.css';
+import React,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [locationSearch,setLocationSearch] = useState('');
+  const [locations,setLocations] = useState<string[]>([]);
+  const disableSearch = locationSearch.trim() === " ";
+  const addLocation = () => {
+    setLocations([locationSearch,...locations]);
+    setLocationSearch("");
+  };
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>Weather App</h1>
     <div>
     <label>
-      Add Location <input type="text" value="Tokyo"/>
+      Add Location <input className= "ml-1 mr-1" type="text" value={locationSearch}
+                     onChange={e => setLocationSearch(e.target.value)}
+    />
     </label>
-     <button>Search</button>
+     <button className="btn btn-primary" onClick={addLocation} disabled={disableSearch}>Search</button>
     </div>
 
     <div>
      <h2>Locations</h2>
-     <table>
+     <table className="table table-hover">
       <thead>
        <tr>
         <th>Name</th>
       </tr>
       </thead>
-     <tbody>
-       <tr><td>Paris</td></tr>
+     <tbody>{
+       locations.map((location,index) =>
+       <tr key={index}><td>{location}</td></tr>)}
      </tbody>
      </table>
     </div>
